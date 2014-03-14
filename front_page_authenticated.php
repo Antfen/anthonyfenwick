98682092b9f66f13f7cb93c3b1905b0456570b6d
Antfen <ant_ajax@hotmail.co.uk>
2014-03-14 17:25:56 +0000

    initial commit

---
+++ front_page_authenticated.php
@@ -0,0 +1,38 @@
+<style>div.node h2.title {
+display:none;
+}
+.meta {
+display:none;
+}
+div.node .links {
+display:none;
+}
+</style>
+
+<div id="concepts_home_page">
+<h1 class="concepts">&nbsp;</h1>
+<!--h1>Concepts...</h1-->
+<p class="artinfo2"><strong>L'exhumation du soldat inconnu - France 1993</strong> <br />
+    (The Exhumation of the Unknown Sodier - France 1993)<br />
+    Large scale installation, paint on panel.</p>
+  <a href="node/3"><img src="sites/all/images/concept_1.jpg" alt="" width="75" height="75" border="0" class="img_concepts_hm_pg" title="" /></a><br class="clear_float" />
+  <a href="node/5"><img src="sites/all/images/concept_2.jpg" alt="" width="75" height="75" border="0" class="img_concepts_hm_pg_alt" title="" /></a>
+  <p class="artinfo"><strong>The XIVth Station</strong><br />
+    Dyptych - paint on panel<br />
+    Hertford Stations of the Cross Public Installation.</p>
+  <br class="clear_float" />
+
+<p class="artinfo2"><strong>Pozieres</strong> <br />
+    Video<br />
+    Combines realtime location and art .</p>
+<a href="node/4"><img src="sites/all/images/concept_3.jpg" alt="" width="75" height="75" border="0" class="img_concepts_hm_pg" title="" /></a> <br class="clear_float" />
+<a href="node/19"><img src="sites/all/images/concept_4.jpg" alt="" width="75" height="75" border="0" class="img_concepts_hm_pg_alt" title="" /></a>
+  <p class="artinfo"><strong>Commuters - London</strong><br />
+    Arylic wash on paper.<br />
+  </p>
+  <br class="clear_float" />
+
+<p class="artinfo2"><strong>Drawing</strong> <br />
+    Pencil / charcoal<br />
+    Completed pieces and preparatory.</p>
+<a href="node/20"><img src="sites/all/images/concept_5.jpg" alt="" width="75" height="75" border="0" class="img_concepts_hm_pg" title="" /></a> <br class="clear_float" /></div>
